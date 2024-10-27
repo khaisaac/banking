6188bb338f8eac9c4a09a09ebf50ba32a179334c
Khaisar Rizki Maulana <khaisarrmaulana@unbaja@ac.id>
2024-10-27 20:17:50 +0700

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Khai",
+    lastName: "Rizz",
+    email: "khaisarrmaulana@unbaja.ac.id",
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Acces and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={100}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSideBar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123 }, { currentBalance: 231 }]}
+      />
+    </section>
+  );
 };
 
 export default Home;
